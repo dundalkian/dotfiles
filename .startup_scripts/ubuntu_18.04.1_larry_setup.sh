e19dc810a231e65ab41d5f7e7e02460e04c5b381
Larry <larrysteele117@gmail.com>
2018-07-28 13:22:37 -0400

    Added .gitignore, setting up install scripts

---
+++ .startup_scripts/ubuntu_18.04.1_larry_setup.sh
@@ -0,0 +1,18 @@
+# This portion will have to be run by hand, but still walks through the process.
+# Creating a custom iso at some point for bionicdog will remove this requirement.
+
+
+# 1. Get an internet connection
+# 2. starts as root so ...
+'''
+apt update
+apt install git
+cd /home
+git clone https://github.com/dundalkian/dotfiles.git
+'''
+# enter github username
+# enter github password (hint, the less patriotic one)
+'''
+cd dotfiles
+'''
+echo hello
