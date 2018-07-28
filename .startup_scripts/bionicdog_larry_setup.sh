c29525e0caa67435d1fe7fe491eb5228f0d2e0ef
Larry <larrysteele117@gmail.com>
2018-07-28 13:30:03 -0400

    updated .gitignore to remove history files

--- .startup_scripts/bionicdog_larry_setup.sh
+++ .startup_scripts/bionicdog_larry_setup.sh
@@ -1,18 +1,29 @@
 # This portion will have to be run by hand, but still walks through the process.
 # Creating a custom iso at some point for bionicdog will remove this requirement.
 
-
 # 1. Get an internet connection
 # 2. starts as root so ...
-'''
-apt update
-apt install git
-cd /home
-git clone https://github.com/dundalkian/dotfiles.git
-'''
+
+#apt update
+#apt install git
+#cd /home/puppy
+#git clone https://github.com/dundalkian/dotfiles.git
 # enter github username
 # enter github password (hint, the less patriotic one)
-'''
-cd dotfiles
-'''
-echo hello
+#cd dotfiles/.startup_scripts
+#sh bionicdog_larry_setup.sh
+
+
+# Shall we begin?
+
+# Needed to symlink all of the configuration files
+apt install stow
+
+
+# Packages not required by install 
+apt install zsh man-db
+
+
+
+
+
